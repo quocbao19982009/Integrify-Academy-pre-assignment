6e85da2a4f6402893915cb861b88b3b96f821898
Bao Nguyen <86527891+quocbao19982009@users.noreply.github.com>
2022-01-18 14:11:35 +0200

    adding favicon

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,19 @@ import NoFoundScreen from "./screens/NoFoundScreen";
 
 function App() {
   return (
-    <Container className="App my-5">
-      <Routes>
-        <Route index element={<MainScreen />} />
-        <Route path="search/:keyword" element={<MainScreen />} />
-        <Route path={"details/:id"} element={<DetailsScreen />} />
-        <Route path="*" element={<NoFoundScreen />} />
-      </Routes>
-    </Container>
+    <>
+      <Container className="App my-5">
+        <Routes>
+          <Route index element={<MainScreen />} />
+          <Route path="search/:keyword" element={<MainScreen />} />
+          <Route path={"details/:id"} element={<DetailsScreen />} />
+          <Route path="*" element={<NoFoundScreen />} />
+        </Routes>
+      </Container>
+      <footer className="text-center mb-5">
+        Made By <a href="http://baonguyendev.com">Bao Nguyen</a>
+      </footer>
+    </>
   );
 }
 
